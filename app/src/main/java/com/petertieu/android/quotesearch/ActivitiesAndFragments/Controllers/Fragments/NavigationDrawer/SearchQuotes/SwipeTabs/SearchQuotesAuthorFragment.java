02169d97a62586e5cfbaf58b4e39783370729af1
Peter Tieu <tieu.peter.v@gmail.com>
2018-06-27 03:20:13 +1000

    Created SearchQuotesAuthorFragment Swipe Tab fragment class for the SearchQuotes navigation drawer fragment

---
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/SearchQuotes/SwipeTabs/SearchQuotesAuthorFragment.java
@@ -0,0 +1,39 @@
+package com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotes.SwipeTabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.petertieu.android.quotesearch.R;
+
+
+
+public class SearchQuotesAuthorFragment extends Fragment {
+
+    //Log for Logcat
+    private final String TAG = "SearchAuthorFragment";
+
+
+
+
+    //Override the onCreateView(..) fragment lifecycle callback method
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle savedInstanceState) {
+        super.onCreateView(layoutInflater, viewGroup, savedInstanceState);
+
+        //Log in Logcat
+        Log.i(TAG, "onCreateView(..) called");
+
+        View view = layoutInflater.inflate(R.layout.fragment_search_quotes_author, viewGroup, false);
+
+
+
+
+        return view;
+    }
+
+
+}
