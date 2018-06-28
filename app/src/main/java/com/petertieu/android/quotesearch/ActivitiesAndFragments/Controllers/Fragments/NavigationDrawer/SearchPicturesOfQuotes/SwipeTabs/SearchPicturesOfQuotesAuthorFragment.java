f860bebbbc8b8b67ea9c18bc3cfc97cc1942629e
Peter Tieu <tieu.peter.v@gmail.com>
2018-06-28 22:42:42 +0800

    Create SearchPicturesOfQuotesAuthorFragment swipe tab fragment for the SearchPicturesOfQuotes fragment of the navigation drawer

---
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/SearchPicturesOfQuotes/SwipeTabs/SearchPicturesOfQuotesAuthorFragment.java
@@ -0,0 +1,37 @@
+package com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchPicturesOfQuotes.SwipeTabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.petertieu.android.quotesearch.R;
+
+public class SearchPicturesOfQuotesAuthorFragment extends Fragment {
+
+    //Log for Logcat
+    private final String TAG = "SearchKeywordFragment";
+
+
+
+
+    //Override the onCreateView(..) fragment lifecycle callback method
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle savedInstanceState) {
+        super.onCreateView(layoutInflater, viewGroup, savedInstanceState);
+
+        //Log in Logcat
+        Log.i(TAG, "onCreateView(..) called");
+
+        View view = layoutInflater.inflate(R.layout.fragment_search_pictures_of_quotes_author, viewGroup, false);
+
+
+
+
+        return view;
+    }
+
+
+}
