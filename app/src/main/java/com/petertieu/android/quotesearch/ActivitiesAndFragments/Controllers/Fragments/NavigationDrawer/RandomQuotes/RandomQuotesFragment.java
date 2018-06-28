abf314fe79378626741f8572eec9826c2917d248
Peter Tieu <tieu.peter.v@gmail.com>
2018-06-29 00:11:52 +0800

    Create RandomQuotesFragment navigation drawer fragment

---
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/RandomQuotes/RandomQuotesFragment.java
@@ -0,0 +1,34 @@
+package com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.RandomQuotes;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.petertieu.android.quotesearch.R;
+
+public class RandomQuotesFragment extends Fragment {
+
+
+    //Log for Logcat
+    private final String TAG = "LanguageChooserFragment";
+
+
+
+    //Override the onCreateView(..) fragment lifecycle callback method
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle savedInstanceState) {
+        super.onCreateView(layoutInflater, viewGroup, savedInstanceState);
+
+        //Log in Logcat
+        Log.i(TAG, "onCreateView(..) called");
+
+        View view = layoutInflater.inflate(R.layout.fragment_random_quotes, viewGroup, false);
+
+        getActivity().setTitle("Random Quotes");
+
+        return view;
+    }
+}
