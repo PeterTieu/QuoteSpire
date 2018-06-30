e0d47591873fb50c1ebe28de5248375a02b44077
Peter Tieu <tieu.peter.v@gmail.com>
2018-07-01 08:20:27 +1000

    Documented Manifest

--- app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Activities/MainActivity.java
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.petertieu.android.quotesearch;
+package com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Activities;
 
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -8,9 +8,18 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
+
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.Favorites.FavoritesFragment;
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.QuoteOfTheDay.QuoteOfTheDayFragment;
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.RandomQuotes.RandomQuotesFragment;
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchPicturesOfQuotes.SearchPicturesOfQuotesFragment;
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotes.SearchQuotesFragment;
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.Settings.SettingsFragment;
+import com.petertieu.android.quotesearch.R;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
 
     int menuItemNumber;
 
+    private FragmentManager fragmentManager;
+    private Fragment fragment;
+
 
 
 
@@ -26,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_one_fragment);
+        setContentView(R.layout.activity_main);
 
 
 
@@ -35,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -44,11 +56,32 @@ public class MainActivity extends AppCompatActivity {
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
+//        getActionBar().setDisplayShowHomeEnabled(true);  // hides action bar icon
+//        getActionBar().setDisplayShowTitleEnabled(true); // hides action bar title
+
+
+//        //Arg #4 and #5: String resources to describe the "open" and "close" drawer actions for accessibiltiy
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.action_settings, R.string.action_settings);
+//        //Set listener to notify of drawer events
+//        mDrawerLayout.setDrawerListener(toggle);
+//        //Synchronise the state of the drawer with the linked DrawerLayout
+//        toggle.syncState();
+
+
+
+        //Return the FragmentManager for interacting with fragments associated with this activity
+        fragmentManager = getSupportFragmentManager();
+
+
+        fragment = new QuoteOfTheDayFragment();
+        fragmentManager.beginTransaction().add(R.id.content_frame, fragment).commit();
 
 
 
 
         NavigationView navigationView = findViewById(R.id.navigation_view);
+
+        //TODO: Make method out of these listener calls
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
@@ -64,11 +97,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-                        //Return the FragmentManager for interacting with fragments associated with this activity
-                        FragmentManager fragmentManager = getSupportFragmentManager();
 
-                        //Find a fragment that was identified by the given ID
-                        Fragment fragment = fragmentManager.findFragmentById(R.id.content_frame);
+
 
 
 
@@ -82,13 +112,9 @@ public class MainActivity extends AppCompatActivity {
                                 menuItemNumber = 1;
 
 
-
-
-
                                 if (fragment != null){
                                     fragmentManager.beginTransaction().remove(fragment).commit();
                                 }
-
 
                                 fragment = new QuoteOfTheDayFragment();
                                 //Start a series of edit operations on the Fragments associated with this FragmentManager
@@ -101,14 +127,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-
-
-
-
-                            case R.id.search_category:
-                                Toast.makeText(getApplicationContext(), "Search Category", Toast.LENGTH_LONG).show();
+                            case R.id.search_quote:
+                                Toast.makeText(getApplicationContext(), "Search Quotes", Toast.LENGTH_LONG).show();
                                 menuItemNumber = 2;
 
 
@@ -117,7 +137,48 @@ public class MainActivity extends AppCompatActivity {
                                 }
 
                                 //Create a fragment
-                                fragment = new SearchCategoryFragment();
+                                fragment = new SearchQuotesFragment();
+                                //Start a series of edit operations on the Fragments associated with this FragmentManager
+                                fragmentManager.beginTransaction().add(R.id.content_frame, fragment).commit();
+
+
+                                return true;
+
+
+
+
+
+                            case R.id.random_quotes:
+                                Toast.makeText(getApplicationContext(), "Random Quote", Toast.LENGTH_LONG).show();
+                                menuItemNumber = 2;
+
+
+                                if (fragment != null){
+                                    fragmentManager.beginTransaction().remove(fragment).commit();
+                                }
+
+                                //Create a fragment
+                                fragment = new RandomQuotesFragment();
+                                //Start a series of edit operations on the Fragments associated with this FragmentManager
+                                fragmentManager.beginTransaction().add(R.id.content_frame, fragment).commit();
+
+
+                                return true;
+
+
+
+
+                            case R.id.search_pictures_of_quotes:
+                                Toast.makeText(getApplicationContext(), "Search Pictures", Toast.LENGTH_LONG).show();
+                                menuItemNumber = 2;
+
+
+                                if (fragment != null){
+                                    fragmentManager.beginTransaction().remove(fragment).commit();
+                                }
+
+                                //Create a fragment
+                                fragment = new SearchPicturesOfQuotesFragment();
                                 //Start a series of edit operations on the Fragments associated with this FragmentManager
                                 fragmentManager.beginTransaction().add(R.id.content_frame, fragment).commit();
 
@@ -129,22 +190,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-                            case R.id.search_author:
-                                Toast.makeText(getApplicationContext(), "Search Author", Toast.LENGTH_LONG).show();
-                                menuItemNumber = 2;
 
 
-                                if (fragment != null){
-                                    fragmentManager.beginTransaction().remove(fragment).commit();
-                                }
-
-                                //Create a fragment
-                                fragment = new SearchAuthorFragment();
-                                //Start a series of edit operations on the Fragments associated with this FragmentManager
-                                fragmentManager.beginTransaction().add(R.id.content_frame, fragment).commit();
-
-
-                                return true;
 
 
 
@@ -168,22 +215,10 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-                            case R.id.random_quote:
-                                Toast.makeText(getApplicationContext(), "Random Quote", Toast.LENGTH_LONG).show();
-                                menuItemNumber = 2;
 
 
-                                if (fragment != null){
-                                    fragmentManager.beginTransaction().remove(fragment).commit();
-                                }
-
-                                //Create a fragment
-                                fragment = new RandomQuotesFragment();
-                                //Start a series of edit operations on the Fragments associated with this FragmentManager
-                                fragmentManager.beginTransaction().add(R.id.content_frame, fragment).commit();
 
 
-                                return true;
 
 
 
@@ -207,17 +242,7 @@ public class MainActivity extends AppCompatActivity {
                                 return true;
 
 
-
-
-
-
-
                         }
-
-
-
-
-
 
 
 
@@ -227,13 +252,33 @@ public class MainActivity extends AppCompatActivity {
 
 
                 });
+
+
+
+
+
+
     }
 
 
 
 
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
 
 
 
@@ -245,6 +290,10 @@ public class MainActivity extends AppCompatActivity {
             case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 return true;
+
+            case R.id.action_settings:
+                return true;
+
         }
         return super.onOptionsItemSelected(item);
     }
