c80f846f538a3b877a4e8b0b6338dffbc53f17c1
Peter Tieu <tieu.peter.v@gmail.com>
2018-07-07 20:52:50 +0300

    Declared instance variables and added corresponding getters and setters

---
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Models/Quote.java
@@ -0,0 +1,56 @@
+package com.petertieu.android.quotesearch.ActivitiesAndFragments.Models;
+
+public class Quote {
+    //States (i.e. instance variables):
+        //isFavorite (boolean)
+        //Quote URL (String)
+        //Picture URL - if exists (String)
+        //QuoteText (String)
+        //Category (String)
+        //Author (String)
+
+
+    String mQuote;
+    int mLenght;
+    String mCategory;
+    String mAuthor;
+    String mId;
+
+
+    public String getQuote(){
+        return mQuote;
+    }
+
+    public void setQuote(String quote){
+        mQuote = quote;
+    }
+
+    public int getLength(){
+        return mLenght;
+    }
+
+    public String getCategory(){
+        return mCategory;
+    }
+
+    public void setCategory(String category){
+        mCategory = category;
+    }
+
+    public String getAuthor(){
+        return mAuthor;
+    }
+
+    public void setAuthor(String author){
+        mAuthor = author;
+    }
+
+    public String getId(){
+        return mId;
+    }
+
+    public void setId(String id){
+        mId = id;
+    }
+
+}
