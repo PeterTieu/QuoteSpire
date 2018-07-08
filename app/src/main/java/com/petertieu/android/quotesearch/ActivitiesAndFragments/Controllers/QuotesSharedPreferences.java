aedaa79b01d9fbd20d188d703ac1a6f3eac1957c
Peter Tieu <tieu.peter.v@gmail.com>
2018-07-08 23:55:37 +1000

    Created SharedPreferences class to store state of push notification and ID of the last Quote Of The Day

---
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/QuotesSharedPreferences.java
@@ -0,0 +1,47 @@
+package com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+public class QuotesSharedPreferences {
+
+    //Declare key
+    private static final String LATEST_QUOTE_OF_THE_DAY_ID = "latestQuoteOfTheDayID";
+    private static final String PUSH_NOTIFICATION_STATE = "pushNotificationState";
+
+
+
+
+
+    public static void setQutoeOfTheDayId(Context context, String latestQuoteOfTheDayId){
+        PreferenceManager.getDefaultSharedPreferences(context)
+                         .edit()
+                         .putString(LATEST_QUOTE_OF_THE_DAY_ID, latestQuoteOfTheDayId)
+                         .apply();
+    }
+
+    public static String getQuoteOfTheDayId(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                                .getString(LATEST_QUOTE_OF_THE_DAY_ID, null);
+    }
+
+
+
+
+
+    public static void setPushNotificationState(Context context, boolean pushNotificationState){
+        PreferenceManager.getDefaultSharedPreferences(context)
+                         .edit()
+                         .putBoolean(PUSH_NOTIFICATION_STATE, pushNotificationState)
+                         .apply();
+    }
+
+
+    public static boolean getPushNotificationState(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                                .getBoolean(PUSH_NOTIFICATION_STATE, false);
+    }
+
+
+
+}
