c7501cb67488070d2188c09594e6d60e9771057b
Peter Tieu <tieu.peter.v@gmail.com>
2018-07-11 23:01:19 +0800

    Created SearchQuotesFragmentPagerAdapter class

---
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/SearchQuotes/SearchQuotesFragmentPagerAdapter.java
@@ -0,0 +1,72 @@
+package com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotes;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotes.SwipeTabs.SearchQuotesAdvancedFragment;
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotes.SwipeTabs.SearchQuotesAuthorFragment;
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotes.SwipeTabs.SearchQuotesCategoryFragment;
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotes.SwipeTabs.SearchQuotesKeywordFragment;
+
+
+//Adapter for the ViewPager of the "Search Quotes" navigation drawer fragment item.
+// Function is to display the Swipe Tabs fragments: "Keyword", "Category", "Author", "Advanced"
+public class SearchQuotesFragmentPagerAdapter extends FragmentPagerAdapter {
+
+
+    //================= Declare INSTANCE VARIABLES ==============================================================
+    //Number of Swipe Tabs fragments
+    private static final int FRAGMENT_COUNT = 4;
+
+
+    //================= Define METHODS ===========================================================================
+    //Constructor
+    public SearchQuotesFragmentPagerAdapter(FragmentManager fragmentManager){
+        super(fragmentManager);
+    }
+
+
+    //Override getItem(..) method from FragmentPagerAdapter
+    @Override
+    public Fragment getItem(int position){
+
+        //Scan through the position numbers of the Swipe Tabs selected, and return the associated Fragments
+        switch (position){
+            case 0:
+                return new SearchQuotesKeywordFragment();
+            case 1:
+                return new SearchQuotesCategoryFragment();
+            case 2:
+                return new SearchQuotesAuthorFragment();
+            case 3:
+                return new SearchQuotesAdvancedFragment();
+        }
+        return null;
+    }
+
+
+    //Override getCount() method from FragmentPagerAdapter
+    @Override
+    public int getCount() {
+        return FRAGMENT_COUNT;
+    }
+
+
+    //Override getPageTitle(..) method from FragmentPagerAdapter - to assign the page titles of the Swipe Tabs selected
+    @Override
+    public CharSequence getPageTitle(int position){
+        switch (position){
+            case 0:
+                return "Keyword";
+            case 1:
+                return "Category";
+            case 2:
+                return "Author";
+            case 3:
+                return "Advanced";
+        }
+        return null;
+    }
+
+}
