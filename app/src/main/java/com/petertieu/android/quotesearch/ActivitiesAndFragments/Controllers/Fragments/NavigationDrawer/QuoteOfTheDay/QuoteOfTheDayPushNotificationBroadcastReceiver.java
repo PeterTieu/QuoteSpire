b9957204c4788ce9fb2647e296885ebc0471622d
Peter Tieu <tieu.peter.v@gmail.com>
2018-07-14 23:41:23 +1000

    Created standalone BroadcastReceiver class to receive broadcast intents from the app for push notifications

---
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/QuoteOfTheDay/QuoteOfTheDayPushNotificationBroadcastReceiver.java
@@ -0,0 +1,43 @@
+package com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.QuoteOfTheDay;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
+
+
+
+
+public class QuoteOfTheDayPushNotificationBroadcastReceiver extends BroadcastReceiver{
+
+    private static final String TAG = "QODPushNotBR";
+
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+
+        if (getResultCode() != Activity.RESULT_OK){
+            return;
+        }
+
+
+
+        int requestCode = intent.getIntExtra(QuoteOfTheDayIntentService.ORDERED_BROADCAST_INTENT_REQUEST_CODE, 0);
+
+        Notification notification = (Notification) intent.getParcelableExtra(QuoteOfTheDayIntentService.PUSH_NOTIFICATION_REQUEST_CODE);
+
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+
+        notificationManager.notify(requestCode, notification);
+
+
+    }
+
+
+
+
+}
