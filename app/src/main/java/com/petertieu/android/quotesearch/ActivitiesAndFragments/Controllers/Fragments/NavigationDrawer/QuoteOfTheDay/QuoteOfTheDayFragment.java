1f2f69684910deb14440615e770c695d57183baa
Peter Tieu <tieu.peter.v@gmail.com>
2018-07-16 23:54:55 +1000

    Added code to display data for QOD Author Quote and QOD Category Quote

--- app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/QuoteOfTheDay/QuoteOfTheDayFragment.java
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/QuoteOfTheDay/QuoteOfTheDayFragment.java
@@ -1,32 +1,50 @@
 package com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.QuoteOfTheDay;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Activities.IntroActivity;
 import com.petertieu.android.quotesearch.ActivitiesAndFragments.Models.Quote;
 import com.petertieu.android.quotesearch.R;
-
-import org.w3c.dom.Text;
-
-import java.util.List;
 
 public class QuoteOfTheDayFragment extends DynamicBroadcastReceiver{
 
     //Log for Logcat
     private final String TAG = "QuoteOfTheDayFragment";
 
+
+
+
     private Quote mQuoteOfTheDay = new Quote();
 
     private TextView mQuoteOfTheDayQuote;
     private TextView mQuoteOfTheDayAuthor;
     private TextView mQuoteOfTheDayCategory;
+
+
+
+
+
+    private Quote mQuoteOfTheDayAuthorQuote = new Quote();
+
+    private TextView mQuoteOfTheDayAuthorQuoteQuote;
+    private TextView mQuoteOfTheDayAuthorQuoteAuthor;
+    private TextView mQuoteOfTheDayAuthorQuoteCategory;
+
+
+
+
+
+    private Quote mQuoteOfTheDayCategoryQuote = new Quote();
+
+    private TextView mQuoteOfTheDayCategoryQuoteQuote;
+    private TextView mQuoteOfTheDayCategoryQuoteAuthor;
+    private TextView mQuoteOfTheDayCategoryQuoteCategory;
 
 
 
@@ -38,7 +56,7 @@ public class QuoteOfTheDayFragment extends DynamicBroadcastReceiver{
 
         setRetainInstance(true);
 
-        new GetQuoteOfTheDayAsyncTask().execute();
+//        new GetQuoteOfTheDayAsyncTask().execute();
     }
 
 
@@ -55,10 +73,19 @@ public class QuoteOfTheDayFragment extends DynamicBroadcastReceiver{
         View view = layoutInflater.inflate(R.layout.fragment_quote_of_the_day, viewGroup, false);
 
         mQuoteOfTheDayQuote = (TextView) view.findViewById(R.id.quote_of_the_day_quote);
-
         mQuoteOfTheDayCategory = (TextView) view.findViewById(R.id.quote_of_the_day_category);
-
         mQuoteOfTheDayAuthor = (TextView) view.findViewById(R.id.quote_of_the_day_author);
+
+
+        mQuoteOfTheDayAuthorQuoteQuote = (TextView) view.findViewById(R.id.quote_of_the_day_author_quote_quote);
+        mQuoteOfTheDayAuthorQuoteAuthor = (TextView) view.findViewById(R.id.quote_of_the_day_author_quote_author);
+        mQuoteOfTheDayAuthorQuoteCategory = (TextView) view.findViewById(R.id.quote_of_the_day_author_quote_category);
+
+
+        mQuoteOfTheDayCategoryQuoteQuote = (TextView) view.findViewById(R.id.quote_of_the_day_category_quote_quote);
+        mQuoteOfTheDayCategoryQuoteAuthor = (TextView) view.findViewById(R.id.quote_of_the_day_category_quote_author);
+        mQuoteOfTheDayCategoryQuoteCategory = (TextView) view.findViewById(R.id.quote_of_the_day_category_quote_category);
+
 
 
         displayQuoteOfTheDay();
@@ -77,36 +104,36 @@ public class QuoteOfTheDayFragment extends DynamicBroadcastReceiver{
 
 
 
-    private class GetQuoteOfTheDayAsyncTask extends AsyncTask<Void, Void, Quote>{
-
-
-
-
-        //Build constructor
-        public GetQuoteOfTheDayAsyncTask(){
-        }
-
-
-
-        @Override
-        protected Quote doInBackground(Void... params){
-            return new GetQuoteOfTheDay().getQuoteOfTheDay();
-        }
-
-
-        @Override
-        protected void onPostExecute(Quote quoteOfTheDay){
-            mQuoteOfTheDay = quoteOfTheDay;
-
-            Log.i(TAG, "Quote of the day - Quote: " + mQuoteOfTheDay.getQuote());
-            Log.i(TAG, "Quote of the day - Category: " + mQuoteOfTheDay.getCategory());
-            Log.i(TAG, "Quote of the day - Author: " + mQuoteOfTheDay.getAuthor());
-            Log.i(TAG, "Quote of the day - ID: " + mQuoteOfTheDay.getId());
-
-
-            displayQuoteOfTheDay();
-        }
-    }
+//    private class GetQuoteOfTheDayAsyncTask extends AsyncTask<Void, Void, Quote>{
+//
+//
+//
+//
+//        //Build constructor
+//        public GetQuoteOfTheDayAsyncTask(){
+//        }
+//
+//
+//
+//        @Override
+//        protected Quote doInBackground(Void... params){
+//            return new GetQuoteOfTheDay().getQuoteOfTheDay();
+//        }
+//
+//
+//        @Override
+//        protected void onPostExecute(Quote quoteOfTheDay){
+//            sQuoteOfTheDay = quoteOfTheDay;
+//
+//            Log.i(TAG, "Quote of the day - Quote: " + sQuoteOfTheDay.getQuote());
+//            Log.i(TAG, "Quote of the day - Category: " + sQuoteOfTheDay.getCategory());
+//            Log.i(TAG, "Quote of the day - Author: " + sQuoteOfTheDay.getAuthor());
+//            Log.i(TAG, "Quote of the day - ID: " + sQuoteOfTheDay.getId());
+//
+//
+//            displayQuoteOfTheDay();
+//        }
+//    }
 
 
 
@@ -121,12 +148,82 @@ public class QuoteOfTheDayFragment extends DynamicBroadcastReceiver{
 
             Log.i(TAG, "called");
 
-            mQuoteOfTheDayQuote.setText(mQuoteOfTheDay.getQuote());
-            mQuoteOfTheDayCategory.setText(mQuoteOfTheDay.getCategory());
-            mQuoteOfTheDayAuthor.setText(mQuoteOfTheDay.getAuthor());
+            mQuoteOfTheDay = IntroActivity.sQuoteOfTheDay;
+
+            if (mQuoteOfTheDayQuote != null){
+                mQuoteOfTheDayQuote.setText("\" " + mQuoteOfTheDay.getQuote() + " \"");
+            }
+
+
+            if (mQuoteOfTheDayAuthor != null){
+                mQuoteOfTheDayAuthor.setText("- " + mQuoteOfTheDay.getAuthor());
+            }
+
+
+            if (mQuoteOfTheDayCategory != null){
+                mQuoteOfTheDayCategory.setText("Category: " + mQuoteOfTheDay.getCategory());
+            }
+
+
+
+
+
+
+
+            mQuoteOfTheDayAuthorQuote = IntroActivity.sQuoteOfTheDayAuthorQuote;
+
+            if (mQuoteOfTheDayAuthorQuoteQuote != null){
+                mQuoteOfTheDayAuthorQuoteQuote.setText("\" " + mQuoteOfTheDayAuthorQuote.getQuote() + " \"");
+            }
+
+            if (mQuoteOfTheDayAuthorQuoteAuthor != null){
+                mQuoteOfTheDayAuthorQuoteAuthor.setText("-" + mQuoteOfTheDayAuthorQuote.getAuthor());
+            }
+
+            if (mQuoteOfTheDayAuthorQuoteCategory != null){
+                mQuoteOfTheDayAuthorQuoteCategory.setText("Category: " + mQuoteOfTheDayAuthorQuote.getCategory());
+            }
+
+
+
+
+
+
+
+
+            mQuoteOfTheDayCategoryQuote = IntroActivity.sQuoteOfTheDayCategoryQuote;
+
+            if (mQuoteOfTheDayCategoryQuote == null){
+                Toast.makeText(getContext(), "Does not exist", Toast.LENGTH_LONG).show();
+            }
+
+            if (IntroActivity.sQuoteOfTheDayCategoryQuote == null){
+                Toast.makeText(getContext(), "Static Does NOT exist", Toast.LENGTH_LONG).show();
+            }
+
+            if (mQuoteOfTheDayCategoryQuote != null){
+                mQuoteOfTheDayCategoryQuoteQuote.setText("\" " + mQuoteOfTheDayCategoryQuote.getQuote() + " \"");
+            }
+
+            if (mQuoteOfTheDayCategoryQuote != null){
+                mQuoteOfTheDayCategoryQuoteAuthor.setText("-" + mQuoteOfTheDayCategoryQuote.getAuthor());
+            }
+
+            if (mQuoteOfTheDayCategoryQuote != null){
+                mQuoteOfTheDayCategoryQuoteCategory.setText("Category: " + mQuoteOfTheDayCategoryQuote.getCategory());
+            }
+
+
+
+
         }
 
     }
+
+
+
+
+
 
 
 
