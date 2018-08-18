b30d0a3022d79a97b24b36af76e1beae9aadaa54
Peter Tieu <tieu.peter.v@gmail.com>
2018-08-18 21:56:37 +1000

    Created SharedPreferences class for storing the author search query in SearchQuotesByAuthorFragment

---
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/SearchQuotes/SwipeTabs/SearchQuotesByAuthor/SearchQuotesByAuthorSharedPref.java
@@ -0,0 +1,28 @@
+package com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotes.SwipeTabs.SearchQuotesByAuthor;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+
+//A SharedPreferences class used for setting (writing) and getting (reading) search queries to/from the SearchView of the SearchQuotesByAuthorFragment class.
+//NOTE: Values saved into ShredPreferences is retrievable even between device restarts.
+public class SearchQuotesByAuthorSharedPref {
+
+    private static final String SEARCH_QUOTES_BY_AUTHOR_SEARCH_QUERY_KEY = "SearchQuotesByAuthorSearchQueryKey"; //Key for saving and loading search query from the SharedPreferences
+
+
+    //Set (write) the search query into SharedPreferences
+    public static void setSearchQuotesByAuthorStoredQuery(Context context, String searchQuotesByAuthorSearchQuery){
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(SEARCH_QUOTES_BY_AUTHOR_SEARCH_QUERY_KEY, searchQuotesByAuthorSearchQuery)
+                .apply();
+    }
+
+
+    //Get (read) the search query from SharedPreferences
+    public static String getSearchQuotesByAuthorStoredQuery(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(SEARCH_QUOTES_BY_AUTHOR_SEARCH_QUERY_KEY, null);
+    }
+}
