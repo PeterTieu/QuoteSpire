c9aeb6f7c78afaec0b91e42dc1cd844db9a05f05
Peter Tieu <tieu.peter.v@gmail.com>
2018-08-18 18:54:59 +1000

    Added "Clear Recent Search Queries" option to SettingsFragment

--- app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/Settings/SettingsFragment.java
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/Settings/SettingsFragment.java
@@ -1,17 +1,30 @@
 package com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.Settings;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.QuoteOfTheDay.QuoteOfTheDayIntentService;
-import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.QuotesSharedPreferences;
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.QuoteOfTheDay.QuoteOfTheDaySharedPreferences;
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotes.SwipeTabs.SearchQuotesByAdvanced.SearchQuotesByAdvancedFragment;
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotes.SwipeTabs.SearchQuotesByAuthor.SearchQuotesByAuthorSharedPref;
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotes.SwipeTabs.SearchQuotesByCategory.SearchQuotesByCategorySharedPref;
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotes.SwipeTabs.SearchQuotesByKeyword.SearchQuotesByKeywordSharedPref;
 import com.petertieu.android.quotesearch.R;
+
+import org.w3c.dom.Text;
 
 public class SettingsFragment extends Fragment {
 
@@ -20,6 +33,7 @@ public class SettingsFragment extends Fragment {
     private final String TAG = "SettingsFragment";
 
     private Switch mPushNotificationSwitch;
+    private TextView mClearAllRecentSearchQueries;
 
 
 
@@ -34,9 +48,11 @@ public class SettingsFragment extends Fragment {
         View view = layoutInflater.inflate(R.layout.fragment_settings, viewGroup, false);
 
         mPushNotificationSwitch = (Switch) view.findViewById(R.id.push_notification_switch);
+        mClearAllRecentSearchQueries = (TextView) view.findViewById(R.id.clear_all_recent_search_queries);
 
 
-        mPushNotificationSwitch.setChecked(QuotesSharedPreferences.getPushNotificationState(getContext()));
+
+        mPushNotificationSwitch.setChecked(QuoteOfTheDaySharedPreferences.getPushNotificationState(getContext()));
 
 
         mPushNotificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -44,22 +60,83 @@ public class SettingsFragment extends Fragment {
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
 
                 if (isChecked == true){
-                    QuotesSharedPreferences.setPushNotificationState(getContext(), true);
+                    QuoteOfTheDaySharedPreferences.setPushNotificationState(getContext(), true);
 
                     QuoteOfTheDayIntentService.setPushNotificationIntentServiceState(getContext(), true);
 
-                    Log.i(TAG, "Push notification state: " + QuotesSharedPreferences.getPushNotificationState(getContext()));
+                    Log.i(TAG, "Push notification state: " + QuoteOfTheDaySharedPreferences.getPushNotificationState(getContext()));
                 }
                 else{
-                    QuotesSharedPreferences.setPushNotificationState(getContext(), false);
+                    QuoteOfTheDaySharedPreferences.setPushNotificationState(getContext(), false);
 
                     QuoteOfTheDayIntentService.setPushNotificationIntentServiceState(getContext(), false);
 
-                    Log.i(TAG, "Push notification state: " + QuotesSharedPreferences.getPushNotificationState(getContext()));
+                    Log.i(TAG, "Push notification state: " + QuoteOfTheDaySharedPreferences.getPushNotificationState(getContext()));
                 }
 
             }
         });
+
+
+
+
+        mClearAllRecentSearchQueries.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+
+
+
+                TextView dialogTitle = new TextView(getActivity());
+                dialogTitle.setText("Clear Recent Search Queries");
+                dialogTitle.setTextSize(22);
+                dialogTitle.setGravity(Gravity.CENTER);
+                dialogTitle.setTypeface(null, Typeface.BOLD);
+                dialogTitle.setTextColor(getResources().getColor(R.color.orange));
+                dialogTitle.setBackgroundColor(getResources().getColor(R.color.grey));
+
+                String dialogMessage = "Do you wish to clear all recent search queries?";
+
+
+                View dialogFragmentView = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_remove_all_favorite_quotes, null);
+
+                AlertDialog alertDialog = new AlertDialog
+                        .Builder(getActivity())
+                        .setView(dialogFragmentView)
+                        .setCustomTitle(dialogTitle)
+                        .setMessage(dialogMessage)
+                        .setNegativeButton(android.R.string.no, null)
+                        .setPositiveButton(android.R.string.yes,
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        SearchQuotesByKeywordSharedPref.setSearchQuotesByKeywordStoredQuery(getActivity(), null);
+                                        SearchQuotesByAuthorSharedPref.setSearchQuotesByAuthorStoredQuery(getActivity(), null);
+                                        SearchQuotesByCategorySharedPref.setSearchQuotesByCategoryStoredQuery(getActivity(), null);
+
+                                        SearchQuotesByAdvancedFragment.sKeywordSearchQuery = null;
+                                        SearchQuotesByAdvancedFragment.sAuthorSearchQuery = null;
+                                        SearchQuotesByAdvancedFragment.sCategorySearchQuery = null;
+
+                                        Toast.makeText(getActivity(), "Cleared all recent search queries", Toast.LENGTH_LONG).show();
+
+                                    }
+                                })
+                        .create();
+
+                alertDialog.show();
+
+
+
+            }
+        });
+
+
+
+
+
+
+
 
 
 
