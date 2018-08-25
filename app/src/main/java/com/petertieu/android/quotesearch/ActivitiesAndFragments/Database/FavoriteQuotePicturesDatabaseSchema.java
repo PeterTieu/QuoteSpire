6119a4e560687a7e4459bcfae18820c9dac321d1
Peter Tieu <tieu.peter.v@gmail.com>
2018-08-25 22:49:24 +0530

    Created FavoriteQuotePicturesDatabaseSchema

---
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Database/FavoriteQuotePicturesDatabaseSchema.java
@@ -0,0 +1,20 @@
+package com.petertieu.android.quotesearch.ActivitiesAndFragments.Database;
+
+
+//Database class #1:
+//Blueprint for the SQLite Database of Favorite Quotes
+public class FavoriteQuotePicturesDatabaseSchema {
+
+    //STATIC inner clas for setting the database
+    public static final class FavoriteQuotePicturesTable{
+
+        //Name of the table
+        public static final String FAVORITE_QUOTE_PICTURES_TABLE_NAME = "favorite_quote_pictures_table";
+
+        //Inner-inner clas that sets names of the columns of the table
+        public static final class Columns{
+            public static final String ID = "id";
+            public static final String IS_FAVORITE = "is_favorite";
+        }
+    }
+}
