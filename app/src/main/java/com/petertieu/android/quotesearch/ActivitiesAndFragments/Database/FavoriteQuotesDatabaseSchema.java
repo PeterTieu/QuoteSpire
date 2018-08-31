24bfb0ea3cf7320a8f2ed89d00f69b287a046e67
Peter Tieu <tieu.peter.v@gmail.com>
2018-08-31 23:56:18 +1000

    Documented and performed Lint on FavoriteQuotesDatabaseSchema

--- app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Database/FavoriteQuotesDatabaseSchema.java
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Database/FavoriteQuotesDatabaseSchema.java
@@ -1,18 +1,16 @@
 package com.petertieu.android.quotesearch.ActivitiesAndFragments.Database;
 
-import java.util.List;
-
 //Database class #1:
-    //Blueprint for the SQLite Database of Favorite Quotes
+    //Blueprint for the SQLite Database of Favorite QuotePictures
 public class FavoriteQuotesDatabaseSchema {
 
-    //STATIC inner clas for setting the database
+    //STATIC inner class for setting the database
     public static final class FavoriteQuotesTable{
 
         //Name of the table
         public static final String FAVORITE_QUOTES_TABLE_NAME = "favorite_quotes_table";
 
-        //Inner-inner clas that sets names of the columns of the table
+        //Inner-inner class that sets names of the columns of the table
         public static final class Columns{
             public static final String ID = "id";
             public static final String QUOTE_STRING = "quote_string";
