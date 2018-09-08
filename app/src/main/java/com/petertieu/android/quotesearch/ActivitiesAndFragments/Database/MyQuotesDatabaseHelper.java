aaeb9ae2a6052c40f65858b478f7480eb4ebd9f4
Peter Tieu <tieu.peter.v@gmail.com>
2018-09-08 21:26:32 +1000

    Created MyQuotesDatabaseHelper

---
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Database/MyQuotesDatabaseHelper.java
@@ -0,0 +1,7 @@
+package com.petertieu.android.quotesearch.ActivitiesAndFragments.Database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyQuotesDatabaseHelper extends SQLiteOpenHelper {
+}
