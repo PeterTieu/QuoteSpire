3f1aba3cacfdc7464b9564fc8dec042f4406d9df
Peter Tieu <tieu.peter.v@gmail.com>
2018-09-08 21:25:33 +1000

    Created MyQuotesDatabaseCursorWrapper

---
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Database/MyQuotesDatabaseCursorWrapper.java
@@ -0,0 +1,8 @@
+package com.petertieu.android.quotesearch.ActivitiesAndFragments.Database;
+
+import android.database.CursorWrapper;
+
+public class MyQuotesDatabaseCursorWrapper extends CursorWrapper {
+
+
+}
