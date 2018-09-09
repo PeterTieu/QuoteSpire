005b673f1f2835fb447c98b9dd1356361d3e794c
Peter Tieu <tieu.peter.v@gmail.com>
2018-09-08 19:39:01 -1200

    Defined constructor and getQuoteFromMyQuotesDatabase() method in MyQuotesDatabaseCursorWrapper in (Class #3) of the SQLiteDatabase for MyQuotes

--- app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Database/MyQuotesDatabaseCursorWrapper.java
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Database/MyQuotesDatabaseCursorWrapper.java
@@ -1,8 +1,33 @@
 package com.petertieu.android.quotesearch.ActivitiesAndFragments.Database;
 
+import android.database.Cursor;
 import android.database.CursorWrapper;
+
+import com.petertieu.android.quotesearch.ActivitiesAndFragments.Models.Quote;
 
 public class MyQuotesDatabaseCursorWrapper extends CursorWrapper {
 
+
+    public MyQuotesDatabaseCursorWrapper(Cursor cursor){
+        super(cursor);
+    }
+
+
+    public Quote getQuoteFromMyQuotesDatabase(){
+        String id = getString(getColumnIndex(MyQuotesDatabaseSchema.MyQuotesTable.Columns.ID));
+        String quoteString = getString(getColumnIndex(MyQuotesDatabaseSchema.MyQuotesTable.Columns.QUOTE_STRING));
+        String author = getString(getColumnIndex(MyQuotesDatabaseSchema.MyQuotesTable.Columns.AUTHOR));
+        int isFavorite = getInt(getColumnIndex(MyQuotesDatabaseSchema.MyQuotesTable.Columns.IS_FAVORITE));
+
+
+        Quote quote = new Quote(id);
+        quote.setQuote(quoteString);
+        quote.setAuthor(author);
+        quote.setFavorite(isFavorite != 0);
+
+
+        return quote;
+
+    }
 
 }
