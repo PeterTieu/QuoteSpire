981f182e4a010f7229cd33ba7af91123f7380b93
Peter Tieu <tieu.peter.v@gmail.com>
2018-09-13 22:55:10 +1000

    Added SharedPreferences field to indicate whether the QOD Push Notification button has ever been pressed

--- app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/QuoteOfTheDay/QuoteOfTheDaySharedPreferences.java
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/QuoteOfTheDay/QuoteOfTheDaySharedPreferences.java
@@ -6,9 +6,24 @@ import android.preference.PreferenceManager;
 public class QuoteOfTheDaySharedPreferences {
 
     //Declare key
+    private static final String PUSH_NOTIFICATION_SWITCH_ID = "pushNotificationSwitchID";
     private static final String LATEST_QUOTE_OF_THE_DAY_ID = "latestQuoteOfTheDayID";
     private static final String PUSH_NOTIFICATION_STATE = "pushNotificationState";
 
+
+
+
+    public static boolean isPushNotificationSwitchPressed(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(PUSH_NOTIFICATION_SWITCH_ID, false);
+    }
+
+    public static void setPushNotificationSwitchPressed(Context context, boolean isPushNotificationSwitchButtonPressed){
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putBoolean(PUSH_NOTIFICATION_SWITCH_ID, isPushNotificationSwitchButtonPressed)
+                .apply();
+    }
 
 
 
@@ -33,7 +48,7 @@ public class QuoteOfTheDaySharedPreferences {
 
     public static boolean isPushNotificationOn(Context context){
         return PreferenceManager.getDefaultSharedPreferences(context)
-                .getBoolean(PUSH_NOTIFICATION_STATE, false);
+                .getBoolean(PUSH_NOTIFICATION_STATE, true);
     }
 
 
