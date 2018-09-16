f6b194cd1ac0c2dea697d1e6034ff5341b1332ae
Peter Tieu <tieu.peter.v@gmail.com>
2018-09-16 18:33:31 +1000

    Added fields to SharedPreferences: QODUpdateHour and QODUpdateMinutes (with getters and setters). These fields save the time set in SettingsFragment for when to search for a new QOD

--- app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/QuoteOfTheDay/PushNotification/QuoteOfTheDaySharedPreferences.java
+++ app/src/main/java/com/petertieu/android/quotesearch/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/QuoteOfTheDay/PushNotification/QuoteOfTheDaySharedPreferences.java
@@ -8,7 +8,9 @@ public class QuoteOfTheDaySharedPreferences {
     //Declare key
     private static final String PUSH_NOTIFICATION_SWITCH_ID = "pushNotificationSwitchID";
     private static final String LATEST_QUOTE_OF_THE_DAY_ID = "latestQuoteOfTheDayID";
-    private static final String PUSH_NOTIFICATION_STATE = "pushNotificationState";
+    private static final String PUSH_NOTIFICATION_STATE_ID = "pushNotificationStateID";
+    private static final String TIME_PICKER_HOUR_ID = "timePickerHourID";
+    private static final String TIME_PICKER_MINUTE_ID = "timePickerMinuteID";
 
 
 
@@ -48,7 +50,7 @@ public class QuoteOfTheDaySharedPreferences {
 
     public static boolean isPushNotificationOn(Context context){
         return PreferenceManager.getDefaultSharedPreferences(context)
-                .getBoolean(PUSH_NOTIFICATION_STATE, true);
+                .getBoolean(PUSH_NOTIFICATION_STATE_ID, true);
     }
 
 
@@ -56,11 +58,42 @@ public class QuoteOfTheDaySharedPreferences {
     public static void setPushNotificationState(Context context, boolean isPushNotificationOn){
         PreferenceManager.getDefaultSharedPreferences(context)
                          .edit()
-                         .putBoolean(PUSH_NOTIFICATION_STATE, isPushNotificationOn)
+                         .putBoolean(PUSH_NOTIFICATION_STATE_ID, isPushNotificationOn)
                          .apply();
     }
 
 
+
+
+    public static int getQuoteOfTheDayUpdateHour(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getInt(TIME_PICKER_HOUR_ID, 10);
+    }
+
+
+
+    public static void setQuoteOfTheDayUpdateHour(Context context, int quoteOfTheDayUpdateHour){
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putInt(TIME_PICKER_HOUR_ID, quoteOfTheDayUpdateHour)
+                .apply();
+    }
+
+
+
+    public static int getQuoteOfTheDayUpdateMinute(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getInt(TIME_PICKER_MINUTE_ID, 05);
+    }
+
+
+
+    public static void setQuoteOfTheDayUpdateMinute(Context context, int quoteOfTheDayUpdateHour){
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putInt(TIME_PICKER_MINUTE_ID, quoteOfTheDayUpdateHour)
+                .apply();
+    }
 
 
 
