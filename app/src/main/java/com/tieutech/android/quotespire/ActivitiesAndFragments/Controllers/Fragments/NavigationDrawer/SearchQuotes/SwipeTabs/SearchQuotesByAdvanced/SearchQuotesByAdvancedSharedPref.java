641b930f5f1369249cd6a1a088070f414412dec4
Peter Tieu <tieu.peter.v@gmail.com>
2018-09-25 22:54:34 +1000

    Created shared preferences class for saving search quotes by Advanced search queries

---
+++ app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/SearchQuotes/SwipeTabs/SearchQuotesByAdvanced/SearchQuotesByAdvancedSharedPref.java
@@ -0,0 +1,76 @@
+package com.tieutech.android.quotespire.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotes.SwipeTabs.SearchQuotesByAdvanced;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+//A SharedPreferences class used for setting (writing) and getting (reading) search queries to/from the SearchView of the SearchQuotesByAdvancedFragment class.
+//NOTE: Values saved into ShredPreferences is retrievable even between device restarts.
+public class SearchQuotesByAdvancedSharedPref {
+
+//    private static final String SEARCH_QUOTES_BY_ADVANCED_KEYWORD_SEARCH_QUERY_KEY = "SearchQuotesByAdvanceKeyworddSearchQueryKey"; //Key for saving and loading search query from the SharedPreferences
+//    private static final String SEARCH_QUOTES_BY_AUTHOR_SEARCH_QUERY_KEY = "SearchQuotesByAdvancedAuthorSearchQueryKey"; //Key for saving and loading search query from the SharedPreferences
+//    private static final String SEARCH_QUOTES_BY_CATEGORY_SEARCH_QUERY_KEY = "SearchQuotesByAdvancedCategorySearchQueryKey"; //Key for saving and loading search query from the SharedPreferences
+//
+//
+//    //Set (write) the KEYWORD search query into SharedPreferences
+//    public static void setSearchQuotesByAdvancedKeywordStoredQuery(Context context, String searchQuotesByAdvancedKeywordSearchQuery){
+//        PreferenceManager.getDefaultSharedPreferences(context)
+//                .edit()
+//                .putString(SEARCH_QUOTES_BY_ADVANCED_KEYWORD_SEARCH_QUERY_KEY, searchQuotesByAdvancedKeywordSearchQuery)
+//                .apply();
+//    }
+//
+//
+//    //Get (read) the KEYWORD search query from SharedPreferences
+//    public static String getSearchQuotesByAdvancedKeywordStoredQuery(Context context){
+//        return PreferenceManager.getDefaultSharedPreferences(context)
+//                .getString(SEARCH_QUOTES_BY_ADVANCED_KEYWORD_SEARCH_QUERY_KEY, null);
+//    }
+//
+//
+//
+//
+//
+//
+//    //Set (write) the AUTHOR search query into SharedPreferences
+//    public static void setSearchQuotesByAdvancedAuthorStoredQuery(Context context, String searchQuotesByAdvancedAuthorSearchQuery){
+//        PreferenceManager.getDefaultSharedPreferences(context)
+//                .edit()
+//                .putString(SEARCH_QUOTES_BY_AUTHOR_SEARCH_QUERY_KEY, searchQuotesByAdvancedAuthorSearchQuery)
+//                .apply();
+//    }
+//
+//
+//    //Get (read) the AUTHOR search query from SharedPreferences
+//    public static String getSearchQuotesByAdvancedAuthorStoredQuery(Context context){
+//        return PreferenceManager.getDefaultSharedPreferences(context)
+//                .getString(SEARCH_QUOTES_BY_AUTHOR_SEARCH_QUERY_KEY, null);
+//    }
+//
+//
+//
+//
+//
+//
+//
+//
+//    //Set (write) the CATEGORY search query into SharedPreferences
+//    public static void setSearchQuotesByAdvancedCategoryStoredQuery(Context context, String searchQuotesByAdvancedCategorySearchQuery){
+//        PreferenceManager.getDefaultSharedPreferences(context)
+//                .edit()
+//                .putString(SEARCH_QUOTES_BY_CATEGORY_SEARCH_QUERY_KEY, searchQuotesByAdvancedCategorySearchQuery)
+//                .apply();
+//    }
+//
+//
+//    //Get (read) the CATEGORY search query from SharedPreferences
+//    public static String getSearchQuotesByAdvancedCategoryStoredQuery(Context context){
+//        return PreferenceManager.getDefaultSharedPreferences(context)
+//                .getString(SEARCH_QUOTES_BY_CATEGORY_SEARCH_QUERY_KEY, null);
+//    }
+
+
+
+
+
+}
