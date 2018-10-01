5a522e1b192bfb8a23398279a063ff6fa2be17fb
Peter Tieu <tieu.peter.v@gmail.com>
2018-10-01 22:14:00 +0800

    Documented StartupBroadcastReceiver class

--- app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/Settings/PushNotification/StartupBroadcastReceiver.java
+++ app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/Settings/PushNotification/StartupBroadcastReceiver.java
@@ -11,36 +11,23 @@ import com.tieutech.android.quotespire.ActivitiesAndFragments.Controllers.Activi
 //Base class for code that receives and handles broadcast intents sent by Context.sendBroadcast(Intent)
 public class StartupBroadcastReceiver extends BroadcastReceiver{
 
-    private static final String TAG = "StartupBR";
+    private static final String TAG = "StartupBR"; //Tag for Logcat
 
-
-
+    //Override onReceive(..) lifecycle callback method
     @Override
     public void onReceive(Context context, Intent intent){
 
         //Log that the broadcast intent has been received
-        //NOTE: We will expect to see:
-        //"Received broadcast intent: android.intent.action.BOOT_COMPLETED"
-        Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction()); //NOTE: Expected to see: "Received broadcast intent: android.intent.action.BOOT_COMPLETED"
 
-//        boolean isPushNotificationOn = QuoteOfTheDaySharedPreferences.isPushNotificationOn(context);
-//        QuoteOfTheDayIntentService.setPushNotificationIntentServiceState(context, isPushNotificationOn);
-//        QuoteOfTheDaySharedPreferences.setPushNotificationSwitchPressed(sContext, true);
-
-
-
+        //
         boolean isPushNotificationOn = QuoteOfTheDayIntentService.isPushNotificationIntentServiceOn(context);
         QuoteOfTheDayIntentService.setPushNotificationIntentServiceState(context, isPushNotificationOn);
 
-        //Start Service
-        String ACTION_START_SERVICE = "com.petertieu.android.quotesearch.ACTION_START_SERVICE";
-        Intent startIntent = new Intent(context, MyService.class);
-        startIntent.setAction(ACTION_START_SERVICE);
-        context.startService(startIntent);
-
-
-
-
+        //Start Service in order to allow the app to persist in the background
+        String ACTION_START_SERVICE = "com.tieutech.android.quotespire.ACTION_START_SERVICE";
+        Intent serviceIntent = new Intent(context, MyService.class);
+        serviceIntent.setAction(ACTION_START_SERVICE);
+        context.startService(serviceIntent);
     }
-
 }
