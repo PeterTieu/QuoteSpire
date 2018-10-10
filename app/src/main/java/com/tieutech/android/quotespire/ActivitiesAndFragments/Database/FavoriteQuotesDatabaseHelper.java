6b745679ac97305de6b0c00367f53ed46f63cd27
Peter Tieu <tieu.peter.v@gmail.com>
2018-10-11 00:13:54 +1100

    Documented FavoriteQuotesDatabaseHelper

--- app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Database/FavoriteQuotesDatabaseHelper.java
+++ app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Database/FavoriteQuotesDatabaseHelper.java
@@ -8,16 +8,18 @@ import android.database.sqlite.SQLiteOpenHelper;
     //Create the Favorite Quotes SQLite database by overriding methods from the SQLiteHelper class
 public class FavoriteQuotesDatabaseHelper extends SQLiteOpenHelper{
 
-    private static final int VERSION = 1;
-    private static final String DATABASE_NAME = "favorite_quotes_database.db";
+    private static final int VERSION = 1; //Version of the SQLiteDatabase
+    private static final String DATABASE_NAME = "favorite_quotes_database.db"; //Name of the SQLiteDatabase
 
+    //Constructor
     public FavoriteQuotesDatabaseHelper(Context context){
         super(context, DATABASE_NAME, null, VERSION);
     }
 
-
+    //Override onCraate(..) callback method - create the SQLiteDatabase
     @Override
     public void onCreate(SQLiteDatabase sqliteDb){
+        //Define SQLiteDatabase and its columns (which correspond to the member variables of the Quote model)
         sqliteDb.execSQL(
                 "create table " +
                         FavoriteQuotesDatabaseSchema.FavoriteQuotesTable.FAVORITE_QUOTES_TABLE_NAME +
@@ -28,22 +30,14 @@ public class FavoriteQuotesDatabaseHelper extends SQLiteOpenHelper{
                         FavoriteQuotesDatabaseSchema.FavoriteQuotesTable.Columns.AUTHOR + ", " +
                         FavoriteQuotesDatabaseSchema.FavoriteQuotesTable.Columns.IS_FAVORITE +
                         ")"
-
         );
     }
 
 
-
+    //Define what happens when the SQLiteDatabase is upgraded
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion){
         //Do nothing
     }
-
-
-
-
-
-
-
 
 }
