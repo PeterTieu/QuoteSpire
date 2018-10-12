de24b2750a6877bf8d74b95ef62b53ea7b157cef
Peter Tieu <tieu.peter.v@gmail.com>
2018-10-12 20:46:05 +1100

    Documented FavoriteQuotesDatabaseCursorWrapper

--- app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Database/FavoriteQuotesDatabaseCursorWrapper.java
+++ app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Database/FavoriteQuotesDatabaseCursorWrapper.java
@@ -5,6 +5,9 @@ import android.database.CursorWrapper;
 
 import com.tieutech.android.quotespire.ActivitiesAndFragments.Models.Quote;
 
+
+//Database class #3:
+    //Wrapper class for the SQLiteDatabase cursor
 public class FavoriteQuotesDatabaseCursorWrapper extends CursorWrapper{
 
     public FavoriteQuotesDatabaseCursorWrapper(Cursor cursor){
