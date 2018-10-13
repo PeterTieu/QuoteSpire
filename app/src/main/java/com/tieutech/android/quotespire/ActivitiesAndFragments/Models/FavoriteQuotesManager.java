a6cdf3356bbd1eb3def97f0279f515939a67f1a3
Peter Tieu <tieu.peter.v@gmail.com>
2018-10-13 21:56:16 +1100

    Documented methods in FavoriteQuotesManager

--- app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Models/FavoriteQuotesManager.java
+++ app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Models/FavoriteQuotesManager.java
@@ -72,91 +72,92 @@ public class FavoriteQuotesManager {
     //===================== All the following methods are accessed like so: FavoriteQuotesManager.get(context).*method* ================================
     //===================== Their purposes are to QUERY, WRITE and REMOVE (favorite) Quote(s) to/from the SQLiteDatabase database ===============================
 
+    //Obtain the Favorite Quote via its ID - this method is called to check if a Quote EXISTS in the Favorite Quotes database
     public Quote getFavoriteQuote(String id){
+
+        //OBTAIN the CursorWrapper (if the Favorite Quote EXIST in the database)
         FavoriteQuotesDatabaseCursorWrapper favoriteQuotesDatabaseCursorWrapper = queryFavoriteQuotes(FavoriteQuotesDatabaseSchema.FavoriteQuotesTable.Columns.ID + " = ?", new String[]{id.toString()});
 
+        //Try risky task - favoriteQuotesDatabaseCursorWrapper.getCount() could return RuntimeException IF the Favorite Quoes database doesn't exist
         try {
             if (favoriteQuotesDatabaseCursorWrapper.getCount() == 0) {
-
                 return null;
             }
 
+            //At this point, the Quote exists in the Favorite Quotes database
+            favoriteQuotesDatabaseCursorWrapper.moveToFirst(); //Point the CursorWrapper to the row containing the Quote (since this row contains the matching ID)
 
-            favoriteQuotesDatabaseCursorWrapper.moveToFirst();
-
-
-            return favoriteQuotesDatabaseCursorWrapper.getQuoteFromFavoriteQuotesDatabase();
+            return favoriteQuotesDatabaseCursorWrapper.getQuoteFromFavoriteQuotesDatabase(); //Return the Quote
         }
 
         finally {
-
-            favoriteQuotesDatabaseCursorWrapper.close();
+            favoriteQuotesDatabaseCursorWrapper.close(); //Close the Cursor
         }
     }
 
 
 
 
-
-
+    //OBTAIN List of ALL the Quote objects in the Favorite Quotes database
     public List<Quote> getFavoriteQuotes() {
 
-        List<Quote> favoriteQuotes = new ArrayList<>();
+        List<Quote> favoriteQuotes = new ArrayList<>(); //Create List object
 
-        FavoriteQuotesDatabaseCursorWrapper favoriteQuotesDatabaseCursorWrapper = queryFavoriteQuotes(null, null);
+        FavoriteQuotesDatabaseCursorWrapper favoriteQuotesDatabaseCursorWrapper = queryFavoriteQuotes(null, null); //Obtain the CursorWrapper, which could to ANY column/row in the database!
 
+        //Try risky task - favoriteQuotesDatabaseCursorWrapper.moveToFirst() could return RuntimeException IF the Favorite Quoes database doesn't exist
         try {
-            favoriteQuotesDatabaseCursorWrapper.moveToFirst();
+            favoriteQuotesDatabaseCursorWrapper.moveToFirst(); //Point the CursorWrapper to FIRST ROW (whch contains the FIRST Quote in the database)
 
+            //While the Cursor DOES NOT point to the position after the last row (IOW, while the cursor is still pointing to an EXISTING row)
             while (!favoriteQuotesDatabaseCursorWrapper.isAfterLast()) {
-                favoriteQuotes.add(favoriteQuotesDatabaseCursorWrapper.getQuoteFromFavoriteQuotesDatabase());
-                favoriteQuotesDatabaseCursorWrapper.moveToNext();
+                favoriteQuotes.add(favoriteQuotesDatabaseCursorWrapper.getQuoteFromFavoriteQuotesDatabase()); //Obtain the Quote pointed to by the Cursor, then add it to the List of Quotes
+                favoriteQuotesDatabaseCursorWrapper.moveToNext(); //Move the Cursor to the next row
             }
-        } finally {
-            favoriteQuotesDatabaseCursorWrapper.close();
+        }
+        finally {
+            favoriteQuotesDatabaseCursorWrapper.close(); //Close the Cursor
         }
 
-        return favoriteQuotes;
-
-
+        return favoriteQuotes; //Return the List of Favorite Quotes
     }
 
 
 
 
-
+    //ADD a Quote to the Favorite Quotes database
     public void addFavoriteQuote(Quote favoriteQuote){
-
-        ContentValues contentValues = getContentValues(favoriteQuote);
-
-        sSQLiteDatabase.insert(FavoriteQuotesDatabaseSchema.FavoriteQuotesTable.FAVORITE_QUOTES_TABLE_NAME, null, contentValues);
+        ContentValues contentValues = getContentValues(favoriteQuote); //Create a ContentValues object, storing the Quote in it
+        sSQLiteDatabase.insert(FavoriteQuotesDatabaseSchema.FavoriteQuotesTable.FAVORITE_QUOTES_TABLE_NAME, null, contentValues); //Insert the ContentValues object (containing the Quote) into the database
     }
 
 
 
 
-
+    //DELETE a Quote from the Favorite Quotes database
     public void deleteFavoriteQuote(Quote favoriteQuote){
-        String favoriteQuoteId = favoriteQuote.getId();
+        String favoriteQuoteId = favoriteQuote.getId(); //Obtain the unique ID of the Quote
 
-        sSQLiteDatabase.delete(FavoriteQuotesDatabaseSchema.FavoriteQuotesTable.FAVORITE_QUOTES_TABLE_NAME,
-                                FavoriteQuotesDatabaseSchema.FavoriteQuotesTable.Columns.ID + " = ? " ,
-                                new String[]{favoriteQuoteId});
+        //Delete the Quote from the database
+        sSQLiteDatabase.delete(FavoriteQuotesDatabaseSchema.FavoriteQuotesTable.FAVORITE_QUOTES_TABLE_NAME, //Database name
+                                FavoriteQuotesDatabaseSchema.FavoriteQuotesTable.Columns.ID + " = ? " , //Column name (ID)
+                                new String[]{favoriteQuoteId}); //Row value (identified by the String value of the ID)
     }
 
 
 
 
+    //UPDATE the Favorite Quotes database - to accomodate the newly added Quote
     public void updateFavoriteQuotesDatabase(Quote favoriteQuote){
-        String favoriteQuotId = favoriteQuote.getId();
+        String favoriteQuotId = favoriteQuote.getId(); //Obtain the unique ID from the Quote
+        ContentValues contentValues = getContentValues(favoriteQuote); //Create a ContentValue, storing the Quote in it
 
-        ContentValues contentValues = getContentValues(favoriteQuote);
-
+        //Update the Quote (stored in the ContentValues object) from the database
         sSQLiteDatabase.update(
-                FavoriteQuotesDatabaseSchema.FavoriteQuotesTable.FAVORITE_QUOTES_TABLE_NAME,
-                contentValues,
-                FavoriteQuotesDatabaseSchema.FavoriteQuotesTable.Columns.ID + " = ? ",
-                new String[]{favoriteQuotId}
+                FavoriteQuotesDatabaseSchema.FavoriteQuotesTable.FAVORITE_QUOTES_TABLE_NAME, //Database name
+                contentValues, //ContentValuea object (containing the Quote)
+                FavoriteQuotesDatabaseSchema.FavoriteQuotesTable.Columns.ID + " = ? ", //Column name (ID)
+                new String[]{favoriteQuotId} //Row value (identified by the String value of the ID)
         );
     }
 
