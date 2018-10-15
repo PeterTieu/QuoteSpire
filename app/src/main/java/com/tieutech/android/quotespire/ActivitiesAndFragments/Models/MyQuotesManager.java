9a1ad985c331561adc3d34b991dd67c974d9c943
Peter Tieu <tieu.peter.v@gmail.com>
2018-10-15 23:21:07 +1100

    Documented SQLiteDatabase management methods in the MyQuotesManager class

--- app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Models/MyQuotesManager.java
+++ app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Models/MyQuotesManager.java
@@ -52,9 +52,9 @@ public class MyQuotesManager {
             //Create/Retrieve the SINGLETON database (of type SQLiteDatabase)
             //getWritableDatable will:
             //IF: An SQLiteDatabase does NOT exist..
-            //Call the overriden onCreate(SQLiteDatabase) from FavoriteQuotesDatabaseHelper to create the SQLiteDatabase
+            //Call the overriden onCreate(SQLiteDatabase) from MyQuotesDatabaseHelper to create the SQLiteDatabase
             //IF: An SQLiteDatabase EXISTS...
-            //Check the version number of the database and call the overriden onUpgrade(..) from FavoriteQuotesDatabaseHelper to upgrade if necessary
+            //Check the version number of the database and call the overriden onUpgrade(..) from MyQuotesDatabaseHelper to upgrade if necessary
             sSQLiteDatabase = new MyQuotesDatabaseHelper(mContext).getWritableDatabase();
         }
 
@@ -74,91 +74,94 @@ public class MyQuotesManager {
     //===================== All the following methods are accessed like so: MyQuotesManager.get(context).*method* ================================
     //===================== Their purposes are to QUERY, WRITE and REMOVE (my) Quote(s) to/from the SQLiteDatabase database ===============================
 
+    //Obtain the My Quote via its ID - this method is called to check if a Quote EXISTS in the My Quotes database
     public Quote getMyQuote(String id){
+
+        //OBTAIN the CursorWrapper (if the My Quote EXIST in the database)
         MyQuotesDatabaseCursorWrapper myQuotesDatabaseCursorWrapper = queryMyQuotes(MyQuotesDatabaseSchema.MyQuotesTable.Columns.ID + " = ?", new String[]{id.toString()});
 
+        //Try risky task - myQuotesDatabaseCursorWrapper.getCount() could return RuntimeException IF the My Quotes database doesn't exist
         try {
             if (myQuotesDatabaseCursorWrapper.getCount() == 0) {
-
                 return null;
             }
 
+            //At this point, the Quote exists in the My Quotes database
+            myQuotesDatabaseCursorWrapper.moveToFirst(); //Point the CursorWrapper to the row containing the Quote (since this row contains the matching ID)
 
-            myQuotesDatabaseCursorWrapper.moveToFirst();
-
-
-            return myQuotesDatabaseCursorWrapper.getQuoteFromMyQuotesDatabase();
+            return myQuotesDatabaseCursorWrapper.getQuoteFromMyQuotesDatabase(); //Return the Quote
         }
 
         finally {
-
-            myQuotesDatabaseCursorWrapper.close();
+            myQuotesDatabaseCursorWrapper.close(); //Close the Cursor
         }
     }
 
 
 
 
-
-
+    //OBTAIN List of ALL the Quote objects in the My Quotes database
     public List<Quote> getMyQuotes() {
 
-        List<Quote> myQuotes = new ArrayList<>();
+        List<Quote> myQuotes = new ArrayList<>(); //Create List object
 
-        MyQuotesDatabaseCursorWrapper myQuotesDatabaseCursorWrapper = queryMyQuotes(null, null);
+        MyQuotesDatabaseCursorWrapper myQuotesDatabaseCursorWrapper = queryMyQuotes(null, null); //Obtain the CursorWrapper, which could to ANY column/row in the database!
 
+        //Try risky task - MyQuotesDatabaseCursorWrapper.moveToFirst() could return RuntimeException IF the My Quoes database doesn't exist
         try {
-            myQuotesDatabaseCursorWrapper.moveToFirst();
+            myQuotesDatabaseCursorWrapper.moveToFirst(); //Point the CursorWrapper to FIRST ROW (whch contains the FIRST Quote in the database)
 
+            //While the Cursor DOES NOT point to the position after the last row (IOW, while the cursor is still pointing to an EXISTING row)
             while (!myQuotesDatabaseCursorWrapper.isAfterLast()) {
-                myQuotes.add(myQuotesDatabaseCursorWrapper.getQuoteFromMyQuotesDatabase());
-                myQuotesDatabaseCursorWrapper.moveToNext();
+                myQuotes.add(myQuotesDatabaseCursorWrapper.getQuoteFromMyQuotesDatabase()); //Obtain the Quote pointed to by the Cursor, then add it to the List of Quotes
+                myQuotesDatabaseCursorWrapper.moveToNext(); //Move the Cursor to the next row
             }
-        } finally {
-            myQuotesDatabaseCursorWrapper.close();
+        }
+        finally {
+            myQuotesDatabaseCursorWrapper.close(); //Close the Cursor
         }
 
-        return myQuotes;
-
-
+        return myQuotes; //Return the List of My Quotes
     }
 
 
 
 
-
+    //ADD a Quote to the My Quotes database
     public void addMyQuote(Quote myQuote){
 
-        ContentValues contentValues = getContentValues(myQuote);
-
-        sSQLiteDatabase.insert(MyQuotesDatabaseSchema.MyQuotesTable.MY_QUOTES_TABLE_NAME, null, contentValues);
+        ContentValues contentValues = getContentValues(myQuote); //Create a ContentValues object, storing the Quote in it
+        sSQLiteDatabase.insert(MyQuotesDatabaseSchema.MyQuotesTable.MY_QUOTES_TABLE_NAME, null, contentValues); //Insert the ContentValues object (containing the Quote) into the database
     }
 
 
 
 
-
+    //DELETE a Quote from the My Quotes database
     public void deleteMyQuote(Quote myQuote){
-        String myQuoteId = myQuote.getId();
+        String myQuoteId = myQuote.getId(); //Obtain the unique ID of the Quote
 
-        sSQLiteDatabase.delete(MyQuotesDatabaseSchema.MyQuotesTable.MY_QUOTES_TABLE_NAME,
-                MyQuotesDatabaseSchema.MyQuotesTable.Columns.ID + " = ? " ,
-                new String[]{myQuoteId});
+        //Delete the Quote from the database
+        sSQLiteDatabase.delete(MyQuotesDatabaseSchema.MyQuotesTable.MY_QUOTES_TABLE_NAME, //Database name
+                MyQuotesDatabaseSchema.MyQuotesTable.Columns.ID + " = ? " , //Column name (ID)
+                new String[]{myQuoteId}); //Row value (identified by the String value of the ID)
     }
 
 
 
 
+    //UPDATE the My Quotes database - to accomodate the newly added Quote
     public void updateMyQuotesDatabase(Quote myQuote){
-        String myQuotId = myQuote.getId();
 
-        ContentValues contentValues = getContentValues(myQuote);
+        String myQuotId = myQuote.getId(); //Obtain the unique ID from the Quote
+        ContentValues contentValues = getContentValues(myQuote); //Create a ContentValue, storing the Quote in it
 
+        //Update the Quote (stored in the ContentValues object) from the database
         sSQLiteDatabase.update(
-                MyQuotesDatabaseSchema.MyQuotesTable.MY_QUOTES_TABLE_NAME,
-                contentValues,
-                MyQuotesDatabaseSchema.MyQuotesTable.Columns.ID + " = ? ",
-                new String[]{myQuotId}
+                MyQuotesDatabaseSchema.MyQuotesTable.MY_QUOTES_TABLE_NAME, //Database name
+                contentValues, //ContentValuea object (containing the Quote)
+                MyQuotesDatabaseSchema.MyQuotesTable.Columns.ID + " = ? ", //Column name (ID)
+                new String[]{myQuotId} //Row value (identified by the String value of the ID)
         );
     }
 
