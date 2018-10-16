cbe6b9e4bf354a92bd67695ea33b4cfdb8718b1e
Peter Tieu <tieu.peter.v@gmail.com>
2018-10-16 23:46:18 +1100

    Documented FavoriteQuotePicturesDatabaseHelper class

--- app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Database/FavoriteQuotePicturesDatabaseHelper.java
+++ app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Database/FavoriteQuotePicturesDatabaseHelper.java
@@ -5,19 +5,21 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 //Database class #2:
-//Create the Favorite Quotes SQLite database by overriding methods from the SQLiteHelper class
+    //Create the Favorite Quote Pictures SQLite database by overriding methods from the SQLiteHelper class
 public class FavoriteQuotePicturesDatabaseHelper extends SQLiteOpenHelper{
 
-    private static final int VERSION = 1;
-    private static final String DATABASE_NAME = "favorite_quote_pictures_database.db";
+    private static final int VERSION = 1; //Version of the SQLiteDatabase
+    private static final String DATABASE_NAME = "favorite_quote_pictures_database.db"; //Name of the SQLiteDatabase
 
+    //Constructor
     public FavoriteQuotePicturesDatabaseHelper(Context context){
         super(context, DATABASE_NAME, null, VERSION);
     }
 
-
+    //Override onCraate(..) callback method - create the SQLiteDatabase
     @Override
     public void onCreate(SQLiteDatabase sqliteDb){
+        //Define SQLiteDatabase and its columns (which correspond to the member variables of the Quote model)
         sqliteDb.execSQL(
                 "create table " +
                         FavoriteQuotePicturesDatabaseSchema.FavoriteQuotePicturesTable.FAVORITE_QUOTE_PICTURES_TABLE_NAME +
@@ -32,17 +34,10 @@ public class FavoriteQuotePicturesDatabaseHelper extends SQLiteOpenHelper{
     }
 
 
-
+    //Define what happens when the SQLiteDatabase is upgraded
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion){
         //Do nothing
     }
-
-
-
-
-
-
-
 
 }
