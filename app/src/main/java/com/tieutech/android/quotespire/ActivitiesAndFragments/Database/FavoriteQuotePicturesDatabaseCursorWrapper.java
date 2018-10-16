0b4b54ec6339a65cc66e03add84324a2ddca8375
Peter Tieu <tieu.peter.v@gmail.com>
2018-10-17 00:14:17 +1100

    Documented FavoriteQuotePicturesDatabaseCursorWrapper class

--- app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Database/FavoriteQuotePicturesDatabaseCursorWrapper.java
+++ app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Database/FavoriteQuotePicturesDatabaseCursorWrapper.java
@@ -6,26 +6,28 @@ import android.database.CursorWrapper;
 import com.tieutech.android.quotespire.ActivitiesAndFragments.Models.Quote;
 import com.tieutech.android.quotespire.ActivitiesAndFragments.Models.QuotePicture;
 
+//Database class #3:
+    //Wrapper class for the SQLiteDatabase cursor
 public class FavoriteQuotePicturesDatabaseCursorWrapper extends CursorWrapper {
 
+    //Constructor - called by FavoriteQuotesManager to obtain a CursorWrapper so as to point the cursor to the QuotePicture in the Favorite Quote Pictures database
     public FavoriteQuotePicturesDatabaseCursorWrapper(Cursor cursor){
         super(cursor);
     }
 
 
+    //Called by FavoriteQuotesManager to obtain the QuotePicture that is pointed to by the CursorWrapper (FavoriteQuotePicturesDatabaseCursorWrapper), in the Favorite Quote Pictures database
     public QuotePicture getQuotePictureFromFavoriteQuotePicturesDatabase(){
-        String id = getString(getColumnIndex(FavoriteQuotePicturesDatabaseSchema.FavoriteQuotePicturesTable.Columns.ID));
-        int isFavorite = getInt(getColumnIndex(FavoriteQuotePicturesDatabaseSchema.FavoriteQuotePicturesTable.Columns.IS_FAVORITE));
-        String bitmapFilePath = getString(getColumnIndex(FavoriteQuotePicturesDatabaseSchema.FavoriteQuotePicturesTable.Columns.BITMAP_FILE_PATH));
 
+        String id = getString(getColumnIndex(FavoriteQuotePicturesDatabaseSchema.FavoriteQuotePicturesTable.Columns.ID)); //Obtain the ID (in the ID column)
+        int isFavorite = getInt(getColumnIndex(FavoriteQuotePicturesDatabaseSchema.FavoriteQuotePicturesTable.Columns.IS_FAVORITE)); //Obtain the favorite state (in the IS_FAVORITE column)
+        String bitmapFilePath = getString(getColumnIndex(FavoriteQuotePicturesDatabaseSchema.FavoriteQuotePicturesTable.Columns.BITMAP_FILE_PATH)); //Obtain the file path of the Bitmap
 
-        QuotePicture quotePicture = new QuotePicture(id);
-        quotePicture.setFavorite(isFavorite != 0);
-        quotePicture.setQuotePictureBitmapFilePath(bitmapFilePath);
+        QuotePicture quotePicture = new QuotePicture(id); //Create QuotePicture object
+        quotePicture.setFavorite(isFavorite != 0); //Set QuotePicture favorite state to associated member variable of QuotePicture
+        quotePicture.setQuotePictureBitmapFilePath(bitmapFilePath); //Set the file path of the Bitmap to associated member viarble of QuotePicture
 
-
-        return quotePicture;
-
+        return quotePicture; //Return the QuotePicture
     }
 
 }
