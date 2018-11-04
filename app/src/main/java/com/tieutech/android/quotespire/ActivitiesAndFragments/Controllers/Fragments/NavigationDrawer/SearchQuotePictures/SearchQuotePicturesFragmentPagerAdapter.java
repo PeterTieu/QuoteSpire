ac8832aeacb20d566565e4e4a24a0243c73eb63a
Peter Tieu <tieu.peter.v@gmail.com>
2018-11-04 21:22:52 +1100

    Documented SearchQuotePicturesFragmentPagerAdapter class

--- app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/SearchQuotePictures/SearchQuotePicturesFragmentPagerAdapter.java
+++ app/src/main/java/com/tieutech/android/quotespire/ActivitiesAndFragments/Controllers/Fragments/NavigationDrawer/SearchQuotePictures/SearchQuotePicturesFragmentPagerAdapter.java
@@ -7,15 +7,24 @@ import android.support.v4.app.FragmentPagerAdapter;
 import com.tieutech.android.quotespire.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotePictures.SwipeTabs.SearchQPByAuthor.SearchQPByAuthorFragment;
 import com.tieutech.android.quotespire.ActivitiesAndFragments.Controllers.Fragments.NavigationDrawer.SearchQuotePictures.SwipeTabs.SearchQPByCategory.SearchQPByCategoryFragment;
 
+
+//Adapter called by SearchQuotePicturesFragment
+    //Holds the following fragments:
+        //1: SearchQPByCategoryFragment
+        //2: SearchQPByAuthorFragment
 public class SearchQuotePicturesFragmentPagerAdapter extends FragmentPagerAdapter {
 
+    private static final int FRAGMENT_COUNT = 2; //How many fragments/swipe tabs to display
 
-    private static final int FRAGMENT_COUNT = 2;
 
+    //Constructor - called by SearchQuotePicturesFragment
     public SearchQuotePicturesFragmentPagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
+
+
+    //Override getItem(..) method from FragmentPagerAdapter
     @Override
     public Fragment getItem(int position) {
         switch (position){
@@ -27,11 +36,17 @@ public class SearchQuotePicturesFragmentPagerAdapter extends FragmentPagerAdapte
         return null;
     }
 
+
+
+    //Override getCount(..) method from FragmentPagerAdapter
     @Override
     public int getCount() {
         return FRAGMENT_COUNT;
     }
 
+
+
+    //Override getPageTitle(..) method from FragmentPagerAdapter
     @Override
     public CharSequence getPageTitle(int position) {
         switch (position){
@@ -42,4 +57,5 @@ public class SearchQuotePicturesFragmentPagerAdapter extends FragmentPagerAdapte
         }
         return null;
     }
+
 }
